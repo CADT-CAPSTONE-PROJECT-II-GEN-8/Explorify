27e161249cb4df8f8802bcbec9447e3c7c9bc79e
AERISjk <121222781+AERISjk@users.noreply.github.com>
2024-07-17 13:33:10 +0700

    Megre

--- frontend_mobile/lib/screens/home/widgets/search_bar.dart
+++ frontend_mobile/lib/screens/home/widgets/search_bar.dart
@@ -21,11 +21,9 @@ class SearchContainer extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width, // *.7
         padding: const EdgeInsets.all(16),
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           color: AppColor.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(16),
-          ),
+          borderRadius: BorderRadius.circular(24.0),
         ),
         child: Row(
           children: [
